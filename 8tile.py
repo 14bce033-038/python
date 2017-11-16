fd20bd9364ecdc41128630fb2a6883f4e0d897a7
14bce033_038 <33536312+14bce033-038@users.noreply.github.com>
2017-11-16 17:35:56 +0530

    Update 8tile.py

--- 8tile.py
+++ 8tile.py
@@ -3,7 +3,6 @@ from tkinter import *
 from tkinter import messagebox
 import functools
 
-#GUI
 class Tile:
 	value = 0
 	xCoord = 0
@@ -25,14 +24,14 @@ class Tile:
 				print(str(self.xCoord) +" "+ str(self.yCoord) +" "+ str(newX) +" "+ str(newY))
 				Tile8PuzzleBoard.swapTiles(self.xCoord, self.yCoord, newX, newY)
 				return
-
+		messagebox.showinfo("Invalid move", "Only neighbouring tiles of 0 can be moved!")
+		
 	@classmethod
 	def isInRange(self, x, y):
 		if x >= 0 and x < 3 and y >= 0 and y < 3:
 			return True
 		return False
 
-#Backend
 class Tile8Puzzle:
 	"""8TileBoard"""
 	board = [[0 for x in range(3)] for y in range(3)]
@@ -69,6 +68,7 @@ class Tile8PuzzleBoard:
 	gameBoard = [[0 for x in range(3)] for y in range(3)]
 	puzzle = 0
 	GameFrame = 0
+	steps = 0
 
 	def __init__(self):
 		self.puzzle = Tile8Puzzle()
@@ -76,10 +76,13 @@ class Tile8PuzzleBoard:
 		self.GameFrame.title("8Tile Puzzle!")
 		self.GameFrame.geometry("240x257")
 		self.createGUIBoard()
-		self.updateEmptySlot()
 		self.GameFrame.mainloop()
 
 	def slideID(self, tileId):
+		if(tileId == 0):
+			messagebox.showinfo("Invalid Step", "You can't move this tile!")
+			return
+			
 		for tileRow in self.puzzle.board:
 			for tile in tileRow:
 				if tile.value == tileId:
@@ -89,26 +92,22 @@ class Tile8PuzzleBoard:
 	def createGUIBoard(self):
 		for i in range(3):
 			for j in range(3):
-				self.gameBoard[i][j] = Button(self.GameFrame, text=self.puzzle.board[i][j].value, command= functools.partial(self.updateBoard, self.puzzle.board[i][j].value), bg="#ff8f00", height=5, width=10)
+				self.gameBoard[i][j] = Button(self.GameFrame, text=self.puzzle.board[i][j].value, command= functools.partial(self.updateBoard, self.puzzle.board[i][j].value), height=5, width=10)
 				self.gameBoard[i][j].grid(row=i, column=j)
 
-	def updateEmptySlot(self):
-		for i in range(3):
-			for j in range(3):
-				if self.puzzle.board[i][j].value == 0:
-					self.gameBoard[i][j].configure(bg="#000000")
-
 	def updateBoard(self, id):
+		self.steps = self.steps + 1
 		self.slideID(id)
 		self.puzzle.printBoard()
 		if self.puzzle.isGameOver():
-			print("Hurrah! You won the game.")
-			messagebox.showinfo("Win!", "You won the game!")
+			messagebox.askquestion("Won!", "You won the game with" + str(steps) + " steps!\ndo you wanna play again?")
 			self.GameFrame.destroy()
+			if 'yes':
+				Tile8PuzzleBoard()
 
 	def swapTiles(self, x, y, newX, newY):
 		self.gameBoard[x][y].grid(row=newX, column=newY)
 		self.gameBoard[newX][newY].grid(row=x, column=y)
 		self.gameBoard[x][y], self.gameBoard[newX][newY] = self.gameBoard[newX][newY], self.gameBoard[x][y]
-		
-t = Tile8PuzzleBoard()
+	
+Tile8PuzzleBoard()
